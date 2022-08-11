4989286d438ff9d4dcfb437e9cfd823f5f72571e
quanLNA <quanle191th@gmail.com>
2022-08-11 14:43:55 +0700

    new commit

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ const config = require('./config/init');
 const logger = require('./config/logger');
 
 let server;
-mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
-    logger.info('connected to MongoDB');
+
     server = app.listen(config.port, () => {
         logger.info(`listen to port ${config.port}`);
     });
-});
+
 
 const exitHandler = () => {
     if (server) {
